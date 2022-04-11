8989f0648eacebf055caba39eaca7bdc71ac81f7
MLGEnio <eniosefa2@gmail.com>
2022-04-11 21:00:43 +0200

    added more responsive for phones

--- javasc.js
+++ javasc.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
 
 const tl = gsap.timeline({defaults:{ease:"power1.out"}});
 
-tl.to(".text",{y:"0%", duration: 1, stagger: 0.25});
-tl.to(".slider",{y:"-100%", duration: 1.5, delay:0.5});
-tl.to(".intro",{y:"-100%", duration: 1},"-=1");
-tl.fromTo("nav",{opacity:0},{opacity:1, duration: 1})
+tl.to(".text",{y:"0%", duration: 1.5, stagger: 0.25});
+tl.to(".slider",{y:"-200%", duration:2.5, delay:0.5});
+tl.to(".intro",{y:"-200%", duration: 2},"-=2");
+tl.fromTo("nav",{opacity:0},{opacity:1, duration: 1},"-=1")
 tl.fromTo(".entry_content",{opacity:0},{opacity:1, duration: 1},"-=1")
